fc86893734b35e60cc42a1934fec6e49509b0d19
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-05-15 23:01:56 -0300

    Initial commit

---
+++ src/main/java/com/gabriel/jwtspringboot/JwtSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.jwtspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSpringBootApplication.class, args);
+    }
+
+}
